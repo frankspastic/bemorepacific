2f647d4287beb5f796873aa7c189f34945ed769f
Frank Maulit <maulit@gmail.com>
2017-09-03 15:51:39 -0500

    adding bar on top for applications and credits in footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,9 +12,6 @@ jQuery(document).ready(function ($) {
 		$(".preloader").delay(1000).fadeOut("slow");
 	});
 
-
-
-
 	jQuery('.scrollup').click(function () {
 		$("html, body").animate({scrollTop: 0}, 2000);
 		return false;
@@ -26,20 +23,23 @@ jQuery(document).ready(function ($) {
 		}, 1500, 'easeInOutExpo');
 		event.preventDefault();
 	});
-	
-	
+
+
 	jQuery(window).scroll(function () {
 	  var top = jQuery(document).scrollTop();
 		var height = 300;
 	  //alert(batas);
-	  
+
 	  if (top > height) {
-		jQuery('.navbar-fixed-top').addClass('menu-scroll');
+			jQuery('.navbar-fixed-top').addClass('menu-scroll');
 	  } else {
-	   jQuery('.navbar-fixed-top').removeClass('menu-scroll');
+	   	jQuery('.navbar-fixed-top').removeClass('menu-scroll');
+
 	  }
-	});	
- // Scroll up 
+
+
+	});
+ // Scroll up
 
     $(window).scroll(function(){
         if ($(this).scrollTop() > 600) {
@@ -51,8 +51,8 @@ jQuery(document).ready(function ($) {
     $('.scrollup').click(function(){
         $("html, body").animate({ scrollTop: 0 }, 1000);
         return false;
-    }); 
- 
+    });
+
  new WOW().init();
 
 });
